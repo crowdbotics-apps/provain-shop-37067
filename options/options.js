9735345b67a8bb44b1b10b2813e5028a82d7bf0c
Crowdbotics <team@crowdbotics.com>
2022-09-14 00:28:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "provain_shop_37067",
+  url: "https://provain_shop_37067.botics.co",
+  api: "https://provain_shop_37067.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
